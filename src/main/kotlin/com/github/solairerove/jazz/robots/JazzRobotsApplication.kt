ea572a21250914b3996c4eaffb712ba9342bd4e0
solairerove <hax2033@gmail.com>
2017-12-06 18:06:09 +0300

    feat: init structure from spring starter

---
+++ src/main/kotlin/com/github/solairerove/jazz/robots/JazzRobotsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.solairerove.jazz.robots
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JazzRobotsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(JazzRobotsApplication::class.java, *args)
+}
